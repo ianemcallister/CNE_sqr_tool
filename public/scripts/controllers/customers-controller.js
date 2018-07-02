c968451012d3adef061d6e893562391c453c29f2
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2018-07-02 09:27:03 -0700

    Add: Customer List Links to Customer Profile Page

--- public/scripts/controllers/customers-controller.js
+++ public/scripts/controllers/customers-controller.js
@@ -9,8 +9,18 @@ function customersController($scope, $log, customerList, firebaseService, $fireb
 
 	//define view model variable
 	var vm = this;
-	vm.customerList = ['a', 'b', 'c'];
-	vm.testList = $firebaseArray(firebase.database().ref().child('customers'));
+	vm.customerList = $firebaseArray(firebase.database().ref().child('customers'));
+	vm.selectedCustomer = '';
+	vm.testList = ['a', 'b', 'c']; //$firebaseArray(firebase.database().ref().child('customers').child('customer_list'));
+	vm.state = {
+		selected: {
+			customer: {
+				$index: "",
+				$id: "",
+
+			}
+		}
+	};
 
 	//identify where we are
 	$log.info('in the customers controller');	//TODO: TAKE THIS OUT LATER
@@ -33,7 +43,8 @@ function customersController($scope, $log, customerList, firebaseService, $fireb
 			//console.log('got this respons', s);
 			
 			//when the list has been loaded update the variables
-			vm.customerList = s;
+			//vm.customerList = s;
+			vm.customerList = $firebaseArray(firebase.database().ref().child('customers').child('customer_list'));
 
 			//reflect the changes
 			$scope.$apply();
@@ -44,9 +55,49 @@ function customersController($scope, $log, customerList, firebaseService, $fireb
 		});
 	};
 
+	/*
+	*	VIEW MODEL FUNCTIONS GO HERE
+	*
+	*/	
+	//	SELECT CLICKED CUSTOMER
+	vm.selectCustomer = function(index_id) {
+		//define local variables
+		vm.state.selected.customer.$id = index_id;
+		vm.selectedCustomer = $firebaseObject(firebase.database().ref().child('customers').child(vm.customerList[index_id].$id));
+	};
+
+	//	UPDATE THE CUSTOMER RECORD
+	vm.updateCustomer = function() {
+		vm.selectedCustomer.$save().then(function succes(s) {
+			console.log('updated record successfully');
+		}).catch(function error(e) {
+			console.log('error updating record', e);
+		});
+	};
+
+	//	MOVE TO THE NEXT CUSTOMER
+	vm.changeRecord = function(select) {
+		//define local variables
+		var optn = { "prev": 0, "next": 1 };
+		var index_id = vm.state.selected.customer.$id;
+
+		switch(optn[select]) {
+			case 0:
+				//console.log('previous record');
+				vm.selectCustomer(vm.state.selected.customer.$id - 1);
+				break;
+			case 1:
+				//console.log('next record');
+				vm.selectCustomer(vm.state.selected.customer.$id + 1);
+				break;
+			default:
+				break;
+		}
+	};
+
 	//run the test
 
 	//on page load
-	load_customer_list();
+	//load_customer_list();
 
 }
